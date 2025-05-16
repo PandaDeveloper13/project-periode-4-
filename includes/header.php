64322d08d1264ef708f60e458e50d264ade9c625
Shadelio <shadeliocooman69@gmail.com>
2025-05-16 13:23:19 +0100

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -7,8 +7,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Rydr</title>
-    <link rel="stylesheet" href="assets/css/main.css">
-    <link rel="icon" type="image/png" href="assets/images/favicon.ico" sizes="32x32">
+    <link rel="stylesheet" href="/assets/css/main.css">
+    <link rel="icon" type="image/png" href="/assets/images/favicon.ico" sizes="32x32">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
@@ -20,32 +20,44 @@
             Rydr.
         </a>
     </div>
-    <form action="">
-        <input type="search" name="" id="" placeholder="Welke auto wilt u huren?">
+    <form action="/pages/zoeken.php" method="GET">
+        <input type="search" name="brand" id="search" placeholder="Zoek op automerk..." required>
         <img src="/assets/images/icons/search-normal.svg" alt="" class="search-icon">
     </form>
     <nav>
         <ul>
-           <li><a href="/pages/home.php">Home</a></l>
+            <li><a href="/pages/home.php">Home</a></li>
             <li><a href="/pages/ons-aanbod.php">Ons aanbod</a></li>
+            <li><a href="/over-ons">Over ons</a></li>
             <li><a href="#">Hulp nodig?</a></li>
         </ul>
     </nav>
     <div class="menu">
         <?php if(isset($_SESSION['id'])){ ?>
+        <div class="wishlist-icon">
+            <a href="/pages/wishlist.php" title="Mijn favorieten">
+                ♥
+                <?php if(isset($_SESSION['likes']) && count($_SESSION['likes']) > 0): ?>
+                <span class="count"><?= count($_SESSION['likes']) ?></span>
+                <?php endif; ?>
+            </a>
+        </div>
         <div class="account">
-            <img src="assets/images/profil.png" alt="">
+            <div class="profile-container">
+                <img src="/assets/images/profil.png" alt="Profiel">
+                <span class="dropdown-arrow">▼</span>
+            </div>
             <div class="account-dropdown">
                 <ul>
-                    <li><img src="assets/images/icons/setting.svg" alt=""><a href="#">Naar account</a></li>
-                    <li><img src="assets/images/icons/logout.svg" alt=""><a href="/logout">Uitloggen</a></li>
+                    <li><img src="/assets/images/icons/setting.svg" alt=""><a href="/pages/register-form.php">Naar account</a></li>
+                    <li><img src="/assets/images/icons/logout.svg" alt=""><a href="/logout.php">Uitloggen</a></li>
                 </ul>
             </div>
         </div>
         <?php }else{ ?>
-            <a href="" class="button-primary">Start met huren</a>
+            <a href="/pages/register-form.php" class="button-primary">Start met huren</a>
         <?php } ?>
 
     </div>
 </div>
-<div class="content">
+<div class="content"></div>
