3fcea2c90499df8e705a254d4d5d52da4b88673b
Shadelio <shadeliocooman69@gmail.com>
2025-05-20 09:33:38 +0100

    Update 404.php

--- pages/404.php
+++ pages/404.php
@@ -1,6 +1,9 @@
-<?php require "includes/header.php" ?>
+<?php 
+$root_path = $_SERVER['DOCUMENT_ROOT'];
+require $root_path . "/includes/header.php"; 
+?>
 <main>
     <h2>Deze pagina bestaat niet</h2>
     <a href="/">Ga terug naar de homepage</a>
 </main>
-<?php require "includes/footer.php" ?>
+<?php require $root_path . "/includes/footer.php"; ?>
