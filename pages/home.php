ec8632dcc770a57ebf55e87a2fc7cb2a88eadf6b
Shadelio <shadeliocooman69@gmail.com>
2025-05-20 09:32:21 +0100

    Update home.php

--- pages/home.php
+++ pages/home.php
@@ -1,18 +1,21 @@
-<?php require "../includes/header.php"; ?>
+<?php 
+$root_path = $_SERVER['DOCUMENT_ROOT'];
+require $root_path . "/includes/header.php"; 
+?>
     <header>
         <div class="advertorials">
             <div class="advertorial">
                 <h2>Hét platform om een auto te huren</h2>
                 <p>Snel en eenvoudig een auto huren. Natuurlijk voor een lage prijs.</p>
-                <a href="/pages/ons-aanbod.php" class="button-primary">Huur nu een auto</a>
-                <img src="/assets/images/car-rent-header-image-1.png" alt="">
+                <a href="/pages/ons-aanbod.php?category=Sport,SUV" class="button-primary">Huur nu een auto</a>
+                <img src="/assets/images/car-rent-header-image-1.webp" alt="">
                 <img src="/assets/images/header-circle-background.svg" alt="" class="background-header-element">
             </div>
             <div class="advertorial">
                 <h2>Wij verhuren ook bedrijfswagens</h2>
                 <p>Voor een vaste lage prijs met prettig voordelen.</p>
-                <a href="/pages/ons-aanbod.php?type=bedrijfswagen" class="button-primary">Huur een bedrijfswagen</a>
-                <img src="/assets/images/car-rent-header-image-2.png" alt="">
+                <a href="/pages/ons-aanbod.php?category=Sedan,Hatchback" class="button-primary">Huur een bedrijfswagen</a>
+                <img src="/assets/images/car-rent-header-image-2.webp" alt="">
                 <img src="/assets/images/header-block-background.svg" alt="" class="background-header-element">
             </div>
         </div>
@@ -46,4 +49,4 @@
         </div>
     </main>
  
-<?php require "../includes/footer.php"; ?>
+<?php require $root_path . "/includes/footer.php"; ?>
